1bf550556106eed86d040567a5df758676728e12
elojah <swingcastor@gmail.com>
2020-07-22 12:19:11 +0200

    wip

--- pkg/space/area.go
+++ pkg/space/area.go
@@ -3,6 +3,7 @@ package space
 import (
 	fmt "fmt"
 	"math/rand"
+	"sort"
 )
 
 // TileKind is used for map generation.
@@ -113,6 +114,29 @@ const (
 	Right
 )
 
+type Orientation uint64
+
+const (
+	Horizontal Orientation = iota
+	Vertical
+)
+
+func NewOrientationRand() Orientation {
+	if rand.Uint64()%2 == 0 {
+		return Horizontal
+	}
+
+	return Vertical
+}
+
+func (o Orientation) Orthogonal() Orientation {
+	if o == Horizontal {
+		return Vertical
+	}
+
+	return Horizontal
+}
+
 type Path struct {
 	Start    Platform
 	End      Platform
@@ -121,22 +145,102 @@ type Path struct {
 }
 
 func (p Path) Apply(params AreaParams, a *Area) {
+	setFloor := func(x, y uint64) {
+		if x >= 0 && x < params.X &&
+			y >= 0 && y < params.Y {
+			a.Tiles[x][y] = Floor
+		}
+	}
+
 	abs := func(n uint64) uint64 {
 		if n > 0 {
 			return n
 		}
+
 		return -n
 	}
-	orthoLength := abs(p.Start.X-p.End.X) + abs(p.Start.Y-p.End.Y)
-	elbows := rand.Uint64() % (orthoLength / 2)
-	for i := 0; i < elbows; i++ {
-		r := rand.Uint64() % 100
+	deltaX := p.End.X - p.Start.X
+	deltaY := p.End.Y - p.Start.Y
 
+	sumSequence := func(sum int, n uint64) []int {
+		if n == 0 {
+			return []int{}
+		}
+
+		neg := false
+		if sum < 0 {
+			sum = -sum
+			neg = true
+		}
+
+		result := make([]int, n)
+		sample := rand.Perm(int(sum))
+		sample = append(sample[0:n-1], 0, int(sum))
+		sort.Slice(sample, func(i, j int) bool { return sample[i] < sample[j] })
+
+		for i := uint64(0); i < n; i++ {
+			result[i] = sample[i+1] - sample[i]
+			if neg {
+				result[i] = -result[i]
+			}
+		}
+		return result
+	}
+
+	nSubPaths := 2 + (rand.Uint64() % (abs(deltaX) + abs(deltaY)))
+	varianceX := nSubPaths + (rand.Uint64() % deltaX)
+	varianceY := nSubPaths + (rand.Uint64() % deltaY)
+
+	seqX0 := sumSequence(int(deltaX+varianceX), nSubPaths)
+	seqX1 := sumSequence(int(-varianceX), nSubPaths)
+	seqY0 := sumSequence(int(deltaY+varianceY), nSubPaths)
+	seqY1 := sumSequence(int(-varianceY), nSubPaths)
+
+	o := NewOrientationRand()
+	pickSubPath := func(o Orientation) int {
+		var result int
+		r := rand.Uint64() % 2
+		if o == Horizontal {
+			if r == 0 {
+				result = seqX0[0]
+				seqX0 = seqX0[1:]
+			} else {
+				result = seqX1[0]
+				seqX1 = seqX1[1:]
+			}
+		} else {
+			if r == 0 {
+				result = seqY0[0]
+				seqY0 = seqY0[1:]
+			} else {
+				result = seqY1[0]
+				seqY1 = seqY1[1:]
+			}
+		}
+
+		return result
+	}
+
+	currentX := p.Start.X
+	currentY := p.Start.Y
+
+	for i := uint64(0); i < nSubPaths*4; i++ {
+		len := pickSubPath(o)
+		if o == Horizontal {
+			if len > 0 {
+				for j := uint64(0); j < len; j++ {
+					setFloor(currentX+j, currentY)
+				}
+			}
+		}
+		for j := uint64(0); j < len; j++ {
+			setFloor(currentX)
+		}
+		o = o.Orthogonal()
 	}
 }
 
-func generateArea(params AreaParams, start Platform, end Platform) Area {
-
+func generateArea(params AreaParams) Area {
 	a := Area{}
 	a.Tiles = make([][]TileKind, params.X)
 
@@ -166,7 +270,7 @@ func generateArea(params AreaParams, start Platform, end Platform) Area {
 			sequence[i], sequence[j] = sequence[j], sequence[i]
 		})
 
-		for j := 0; j < nPaths; j++ {
+		for j := uint64(0); j < nPaths; j++ {
 			// same platform, try next one and increment nPaths to keep correct count
 			end := sequence[j]
 			if i == end {
