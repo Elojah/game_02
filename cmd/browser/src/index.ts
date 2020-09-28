312a630ffdf070a33a3f78b43c56af4ad41dc21a
elojah <swingcastor@gmail.com>
2020-09-28 16:33:20 +0200

    add form login

--- cmd/browser/src/index.ts
+++ cmd/browser/src/index.ts
@@ -9,10 +9,14 @@ function main() {
   const config: Phaser.Types.Core.GameConfig = {
     title: 'GAME_02',
     type: Phaser.AUTO,
+    parent: 'main',
     scale: {
-      width: window.document.body.clientWidth,
+      width: window.innerWidth,
       height: window.innerHeight,
     },
+    dom: {
+      createContainer: true
+    }
     // physics: {
     //   default: 'arcade',
     //   arcade: {
@@ -20,16 +24,16 @@ function main() {
     //   },
     // },
     // parent: 'game',
-    backgroundColor: '#000000',
+    // backgroundColor: '#000000',
   };
 
   const g = new Phaser.Game(config)
-  const sm = new Phaser.Scenes.SceneManager(g, {})
-  sm.add('menu', new Menu({
+  g.scene.add('menu', new Menu({
     key: 'menu',
     active: true,
     visible: true,
-  }), true)
+  }))
+  g.scene.start('menu')
 };
 
 main();
--- cmd/browser/src/scene/menu.ts
+++ cmd/browser/src/scene/menu.ts
@@ -5,11 +5,15 @@ export class Menu extends Scene {
         super(config);
     }
     preload() {
-        this.load.image('background', 'background_0.png')
+        this.load.html('login', 'html/text_input.html')
+        this.load.image('background', 'background_menu.png')
         this.load.image('theme', 'theme.png')
     }
     create() {
-        this.add.image(400, 300, 'background')
+        this.add.image(0, 0, 'background').setOrigin(0)
+        const login = this.add.dom(0, 0).createFromCache('login').setOrigin(0);
+        login.addListener('click');
+        login.on('click', function () {console.log("hey")})
     }
     update() {}
 }
